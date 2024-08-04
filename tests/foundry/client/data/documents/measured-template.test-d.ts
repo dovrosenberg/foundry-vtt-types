cf5f96074a459aad53f1359c0870eda0c4233d50
Dov Rosenberg <dov@rosenbergmail.com>
2024-08-04 19:22:02 -0400

    couple comment cleanups

--- tests/foundry/client/data/documents/measured-template.test-d.ts
+++ tests/foundry/client/data/documents/measured-template.test-d.ts
@@ -5,5 +5,4 @@ const doc = new MeasuredTemplateDocument();
 expectTypeOf(doc.author).toEqualTypeOf<User | undefined>();
 expectTypeOf(doc.layer).toEqualTypeOf<TemplateLayer>();
 
-// TODO: Modify to MeasuredTemplateConfig | null once data can be grabbed from CONFIG
-expectTypeOf(doc.sheet).toEqualTypeOf<FormApplication | null>();
+expectTypeOf(doc.sheet).toEqualTypeOf<FormApplication | foundry.applications.api.ApplicationV2 | null>();
--- tests/foundry/client/data/documents/user.test-d.ts
+++ tests/foundry/client/data/documents/user.test-d.ts
@@ -19,6 +19,6 @@ expectTypeOf(user._id).toEqualTypeOf<string | null>();
 expectTypeOf(user.avatar).toEqualTypeOf<string | null | undefined>();
 
 // TODO: Modify to ConfiguredDocumentSheet<typeof User> | null once data can be grabbed from CONFIG
-expectTypeOf(user.sheet).toEqualTypeOf<FormApplication | null>();
+expectTypeOf(user.sheet).toEqualTypeOf<FormApplication | foundry.applications.api.ApplicationV2 | null>();
 
 expectTypeOf(user.color).toEqualTypeOf<string>();
